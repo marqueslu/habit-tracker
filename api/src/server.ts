d6d762c8e4fa935dfc2d675652106db53774cee0
Lucas Marques <lucsilva@pagar.me>
2023-01-17 19:46:47 -0300

    add project initial structure

---
+++ api/src/server.ts
@@ -0,0 +1,14 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('HTTP Server running!')
+})
