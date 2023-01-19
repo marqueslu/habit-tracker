e264e771df2cf6056e95e3a132692bb3534a27ee
Lucas Marques <lucsilva@pagar.me>
2023-01-19 16:28:38 -0300

    configure routes and prisma provider

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,11 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
+app.register(appRoutes)
 
 app.listen({
   port: 3333
