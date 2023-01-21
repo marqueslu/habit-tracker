45a9c8bac7ddb56dca14991a26828228ba886198
Lucas Marques <lucsilva@pagar.me>
2023-01-21 16:16:29 -0300

    include the Home screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native'
 import { 
   useFonts, 
   Inter_400Regular, 
@@ -7,7 +7,10 @@ import {
   Inter_800ExtraBold 
 } from '@expo-google-fonts/inter'
 
-import { Loading } from './src/components/Loading';
+import './src/lib/dayjs'
+
+import { Loading } from './src/components/Loading'
+import { Home } from './src/screens/Home'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,22 +21,13 @@ export default function App() {
   })
 
   if(!fontsLoaded) {
-    return <Loading />;
+    return <Loading />
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Home />      
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
