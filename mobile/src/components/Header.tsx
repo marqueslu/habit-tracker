cbcb037c6b2d7cacba21ebd2203410a023dfb0d2
Lucas Marques <lucsilva@pagar.me>
2023-01-29 15:20:18 -0300

    add navigation to Header component

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,18 +1,22 @@
 import { View, Text, TouchableOpacity } from "react-native"
 import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
+import { useNavigation } from "@react-navigation/native"
 
 import Logo from '../assets/logo.svg'
 
 export function Header() {
+  const { navigate } = useNavigation()
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
-      <TouchableOpacity 
+      <TouchableOpacity
         activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+        onPress={() => navigate('new')}
       >
-        <Feather 
+        <Feather
           name="plus"
           color={colors.violet[500]}
           size={20}
