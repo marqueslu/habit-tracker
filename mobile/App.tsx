5e056cd91a563947c445ef78b962408eb5f8fb9c
Lucas Marques <lucsilva@pagar.me>
2023-01-29 15:21:24 -0300

    configuring routes in App

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,32 +1,32 @@
 import { StatusBar } from 'react-native'
-import { 
-  useFonts, 
-  Inter_400Regular, 
-  Inter_600SemiBold, 
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
   Inter_700Bold,
-  Inter_800ExtraBold 
+  Inter_800ExtraBold
 } from '@expo-google-fonts/inter'
 
 import './src/lib/dayjs'
 
 import { Loading } from './src/components/Loading'
-import { Home } from './src/screens/Home'
+import { Routes } from './src/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Inter_400Regular, 
-    Inter_600SemiBold, 
+    Inter_400Regular,
+    Inter_600SemiBold,
     Inter_700Bold,
-    Inter_800ExtraBold 
+    Inter_800ExtraBold
   })
 
-  if(!fontsLoaded) {
+  if (!fontsLoaded) {
     return <Loading />
   }
 
   return (
     <>
-      <Home />      
+      <Routes />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </>
   );
